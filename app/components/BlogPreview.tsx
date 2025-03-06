e29abcb5e58e27bf3f7f095f46279d86e343f51c
AmorZhao <amor_7303@163.com>
2025-03-06 09:48:23 +0000

    add fse note

--- app/components/BlogPreview.tsx
+++ app/components/BlogPreview.tsx
@@ -15,7 +15,7 @@ const BlogPreview = ({ title, description, date, category, tags, link }: BlogPre
       <p><i className="fa-solid fa-calendar"></i> {new Date(date).toLocaleString("en-GB", { dateStyle: "short", timeStyle: "medium" })}</p>
       <p><i className="fa-solid fa-folder-closed"></i> {category}</p>
       </div>
-      <p className="line-clamp-2 ml-1 custom-gray mt-1">{description}</p>
+      <p className="line-clamp-2 ml-1 custom-gray mt-2">{description}</p>
       <div className="ml-1">
       {tags.map((tag, index) => (
         <span key={index} className="text-xs custom-blue mr-2 mt-1">#{tag}</span>
